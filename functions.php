85d2af2dd571e10887a32a4e6e6cbec10cc86fa4
Shpakouski <job.shpakouski@gmail.com>
2020-11-27 04:49:22 +0300

    registering sidebars

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 add_action('after_setup_theme', 'si_setup');
 add_action('wp_enqueue_scripts', 'si_scripts');
+add_action( 'widgets_init', 'si_register' );
 
 add_filter('show_admin_bar', '__return_false');
 
@@ -14,7 +15,7 @@ function si_setup(){
     add_theme_support( 'post-thumbnails' );
 }
 
-function si_scripts() {
+function si_scripts(){
     wp_enqueue_script(
         'si-js',
         _si_assets_path('js/js.js'),
@@ -29,6 +30,51 @@ function si_scripts() {
         '1.0',
         'all'
     );
+}
+
+function si_register(){
+    register_sidebar([
+        'name' => 'Контакты в шапке сайта',
+        'id' => 'si-header',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Контакты в подвале сайта',
+        'id' => 'si-footer',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Сайдбар в футере - Колонка 1',
+        'id' => 'si-footer-column-1',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Сайдбар в футере - Колонка 2',
+        'id' => 'si-footer-column-2',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Сайдбар в футере - Колонка 3',
+        'id' => 'si-footer-column-3',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Карта',
+        'id' => 'si-map',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+    register_sidebar([
+        'name' => 'Сайдбар под картой',
+        'id' => 'si-after-map',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
 }
 
 function _si_assets_path($path){
