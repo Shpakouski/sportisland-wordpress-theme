b74ff90165d4731ad8727389f145097a56cb6587
Shpakouski <job.shpakouski@gmail.com>
2020-11-29 03:57:48 +0300

    widget info

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ $widgets = [
     'widget-text.php',
     'widget-contacts.php',
     'widget-social-links.php',
-    'widget-iframe.php'
+    'widget-iframe.php',
+    'widget-info.php'
 ];
 
 foreach($widgets as $widget){
@@ -91,6 +92,7 @@ function si_register(){
     register_widget('si_widget_contacts');
     register_widget('si_widget_social_links');
     register_widget('si_widget_iframe');
+    register_widget('si_widget_info');
 }
 
 function _si_assets_path($path){
